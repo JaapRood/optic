e7411bcda17b20c4179cee78e2539a18a1f22d88
Jaap Rood <jaap@jaaprood.nl>
2020-11-11 15:42:26 +0100

    Implement diffing interactions through WASM interface

--- workspaces/diff-engine/wasm/src/lib.rs
+++ workspaces/diff-engine/wasm/src/lib.rs
@@ -2,12 +2,12 @@
 
 use wasm_bindgen::prelude::*;
 
-use optic_diff_engine::{SpecEvent, SpecProjection};
+use optic_diff_engine::{HttpInteraction, InteractionDiffResult, SpecEvent, SpecProjection};
 
 #[wasm_bindgen(start)]
 pub fn init() {
-  std::panic::set_hook(Box::new(console_error_panic_hook::hook));
   wasm_logger::init(wasm_logger::Config::default());
+  std::panic::set_hook(Box::new(console_error_panic_hook::hook));
 }
 
 #[wasm_bindgen]
@@ -19,8 +19,31 @@ pub fn spec_from_events(spec_json: String) -> Result<WasmSpecProjection, JsValue
 }
 
 #[wasm_bindgen]
+pub fn diff_interaction(
+  interaction_json: String,
+  spec: &WasmSpecProjection,
+) -> Result<String, JsValue> {
+  let TaggedInput(interaction, tags): TaggedInput<HttpInteraction> =
+    serde_json::from_str(&interaction_json).unwrap();
+
+  let results = spec.diff_interaction(interaction);
+
+  Ok(serde_json::to_string(&results).unwrap())
+}
+
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
+struct TaggedInput<T>(T, Tags);
+type Tags = Vec<String>;
+
+#[wasm_bindgen]
 pub struct WasmSpecProjection {
   projection: SpecProjection,
+}
+
+impl WasmSpecProjection {
+  pub fn diff_interaction(&self, interaction: HttpInteraction) -> Vec<InteractionDiffResult> {
+    optic_diff_engine::diff_interaction(&self.projection, interaction)
+  }
 }
 
 impl From<SpecProjection> for WasmSpecProjection {
--- workspaces/diff-engine/wasm/test.js
+++ workspaces/diff-engine/wasm/test.js
@@ -1,6 +1,7 @@
 const DiffEngine = require('.');
 const Path = require('path');
 const Fs = require('fs-extra');
+const Readline = require('readline');
 
 DiffEngine.init();
 
@@ -8,9 +9,25 @@ let exampleSessionPath = Path.join(
   __dirname,
   '../tests/fixtures/ergast-example-spec.json'
 );
+let exampleInteractionsPath = Path.join(
+  __dirname,
+  '../tests/fixtures/ergast-captures/ergast-simulated-traffic.jsonl'
+);
 
 let rawSpec = Fs.readFileSync(exampleSessionPath);
 
 let spec = DiffEngine.spec_from_events(rawSpec.toString());
 
-console.log(spec);
+async function diffInteractions(source) {
+  let lines = Readline.createInterface({ input: source, crlfDelay: Infinity });
+
+  let i = 0;
+  for await (const line of lines) {
+    const diffs = DiffEngine.diff_interaction(line, spec);
+    console.log(`\n\ndiffs for line ${i}:`);
+    console.log(diffs);
+    i++;
+  }
+}
+
+diffInteractions(Fs.createReadStream(exampleInteractionsPath));
